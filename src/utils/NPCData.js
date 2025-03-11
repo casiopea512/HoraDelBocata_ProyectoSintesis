672d436ce6e07fbf9eb1113257797067b9387c97
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-03-11 23:54:52 +0100

    [personajes] - creación parametrizada de los persoanjes según la escena a partir de una constante.

---
+++ src/utils/NPCData.js
@@ -0,0 +1,18 @@
+export const npcData = {
+
+    Finn: {
+        name: "Finn",
+        textureKey: "FinnFront",
+        scene: "KitchenScene",
+        x: 400,
+        y: 300,
+    },
+
+    BMO:{
+        name: "Bmo",
+        textureKey: "BmoFront",
+        scene: "KitchenScene",
+        x: 500,
+        y: 300,
+    },
+}
