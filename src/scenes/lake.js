a4ff13bd8ab33808d13b5773e73c83ed6b8f016a
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-04-08 17:54:24 +0200

    [ Escena lago - DONE] - creación capa boundaries para el lago - reposicionamiento de los npcs

--- src/scenes/lake.js
+++ src/scenes/lake.js
@@ -10,7 +10,7 @@ export default class Lake extends BaseScene {
         this.load.json("npcDialogs", "/src/utils/dialogs.json");
 
         this.load.image('Water', "/assets/images/tiles/Water.png");
-        this.load.image('TreesPlants', "/assets/images/objects/Basic_Grass_Biom_things.png");
+        this.load.image('Grass', "/assets/images/tiles/Grass.png");
         this.load.image('Hills', "/assets/images/tiles/Hills.png");
         this.load.tilemapTiledJSON('mapa', 'assets/maps/lake.json');
 
@@ -37,29 +37,35 @@ export default class Lake extends BaseScene {
         const map = this.make.tilemap({ key: "mapa" });
 
         const tilesetHills = map.addTilesetImage('Hills', 'Hills');
-        const tilesetWater = map.addTilesetImage('Water', 'Water');
-        const tilesetObjects = map.addTilesetImage('TreesPlants', 'TreesPlants');
+        const tilesetGrass = map.addTilesetImage('Grass', 'Grass');
+        const tilesetObjects = map.addTilesetImage('Water', 'Water');
 
         this.layers = {
-            suelo: map.createLayer("Suelo", tilesetHills, 0, 0).setScale(6),
-            water: map.createLayer("Water", tilesetWater, 0, 0).setScale(6),
-            objetos: map.createLayer("Objetos", tilesetObjects, 0, 0).setScale(6)
+            suelo: map.createLayer("Suelo", [tilesetHills,tilesetGrass], 0, 0).setScale(6),
+            objetos: map.createLayer("Objetos", tilesetObjects, 0, 0).setScale(6),
+            boundaries: map.createLayer("Boundaries", tilesetGrass, 0, 0).setScale(6),
         };
 
-        this.layers.water.setDepth(-1);
         this.layers.suelo.setDepth(0);
-        this.layers.objetos.setDepth(1);
-
-        this.layers.suelo.setCollisionByProperty({ colision: true });
-        this.layers.objetos.setCollisionByProperty({ colision: true });
+        this.layers.objetos.setDepth(-1);
+        this.layers.boundaries.setDepth(-1);
     }
 
     createCollisions() {
+        this.physics.add.collider(this.player.sprite, this.layers.boundaries);
+        this.layers.boundaries.setCollisionByExclusion([-1]);
+        
         this.npcs.forEach(npc => {
             this.physics.add.collider(this.player.sprite, npc.sprite);
         });
-        this.physics.add.collider(this.player.sprite, this.layers.suelo);
-        this.physics.add.collider(this.player.sprite, this.layers.objetos);
-        this.layers.objetos.setCollisionBetween(9,11)
+        // añadir la colisión a las capas
+        // Object.values(this.layers).forEach(layer => {
+        //     this.physics.add.collider(this.player.sprite, layer);
+        //     if(layer.layer.name !== 'Suelo'){
+        //         layer.setCollisionByExclusion([-1]);
+        //     }
+        // });
+        
+        
     }
 }
