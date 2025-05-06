50b74063c41de99cd27ca85c389650e48b194c63
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-05-06 20:00:01 +0200

    [icono sonido - IN PROGRESS] - icono sonido en el game

--- gameOver.php
+++ gameOver.php
@@ -1,6 +1,6 @@
 <?php
-// if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'localhost') === false) {
-if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'https://horadelbocata.ieti.site') === false) {
+if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'localhost') === false) {
+// if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'https://horadelbocata.ieti.site') === false) {
     header('HTTP/1.1 403 Forbidden');
     include 'errors/error403.html';
     exit();
