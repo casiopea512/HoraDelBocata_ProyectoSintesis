d3eb56b6a96814726f5ed666c39b16d2fb28bbc6
Mar <mar.melich@gmail.com>
2025-05-09 19:09:39 +0200

    [FAVICON] - añadido en todas las paginas

--- gameOver.php
+++ gameOver.php
@@ -16,6 +16,7 @@ if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'localh
 
     <link href="styles.css" rel="stylesheet">
     <link href="gameOver.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="/assets/images/objects/sandwich.png">
     
     <title>Final del Juego | Hora del bocata</title>
 </head>
